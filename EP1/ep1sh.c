3dd40557b0d355710914ce0e747a2788c527c682
robotenique <julianogarcia_1997@hotmail.com>
2017-08-10 17:51:00 -0300

    add shell string formatter

--- EP1/ep1sh.c
+++ EP1/ep1sh.c
@@ -9,9 +9,16 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "error.h"
+
 
 #include <readline/readline.h>
 #include <readline/history.h>
+
+char * buildShellString();
+
 /*
  * Function: main
  * --------------------------------------------------------
@@ -23,10 +30,16 @@
  */
 int main(int argc, char const *argv[]) {
     char *inpt;
+    char *fshell = buildShellString();
     while (1) {
         // TODO Print working directory
-        inpt = readline(">>> ");
+        inpt = readline(fshell);
         add_history(inpt);
+        if(!strcmp(inpt,"exit")){
+            free(inpt);
+            free(fshell);
+            exit(0);
+        }
 
         // TODO Split line and execute command
         // Test lines ---------//
@@ -38,4 +51,33 @@ int main(int argc, char const *argv[]) {
         free(inpt);
     }
     return 0;
+}
+
+/*
+ * Function: buildShellString
+ * --------------------------------------------------------
+ * Get the current working directory, and build the shell
+ * initial string in the form '[x]$ ' as required.
+ *
+ * @args
+ *
+ * @return the formmated string for the shell input
+ */
+char * buildShellString(){
+    long size = pathconf(".", _PC_PATH_MAX);
+    char *buf = (char *)emalloc(size);
+    getcwd(buf, size);
+    size = strlen(buf) + 5;
+    char *fshell = emalloc(size);
+    fshell[0]='[';
+    fshell[size - 1] = 0;
+    fshell[size - 2] = ' ';
+    fshell[size - 3] = '$';
+    fshell[size - 4] = ']';
+
+    int i;
+    int j;
+    for (i = 1, j = 0; i < size - 4; fshell[i++] = buf[j++]);
+    free(buf);
+    return fshell;
 }
