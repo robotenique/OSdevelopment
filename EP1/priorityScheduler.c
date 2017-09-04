8d98076f4676865f6bbfb0e68821be436b5ab429
IceMage144 <jgabriel.basi@hotmail.com>
2017-09-04 15:31:21 -0300

    Add dinamic quanta calculation based on priority average and standard deviation

--- EP1/priorityScheduler.c
+++ EP1/priorityScheduler.c
@@ -16,7 +16,9 @@ static pthread_mutex_t gmtx;
 static double *quantum;
 static int finished = 0;
 static int deadlineCompleted = 0;
-
+static double var = 0;
+static double avg = 0;
+static int count = 0;
 
 static void *iWait(void *t) {
     double *dt = (double *)t;
@@ -26,13 +28,20 @@ static void *iWait(void *t) {
     return NULL;
 }
 
+double calcQuanta(double priority) {
+    double L = (priority - avg)/sqrt(var);
+    double scale = 2.5*fmin(4.0, fabs(L));
+    return QUANTUM_VAL*(11.0 - scale);
+}
+
 void *runPScheduler(void *arg) {
     Node *n = (Node *)arg;
     double w;
 
-    while ((w = fmin(n->p->dt, QUANTUM_VAL*quantum[n->p->nLine]))) {
+    while ((w = fmin(n->p->dt, calcQuanta(quantum[n->p->nLine])))) {
         pthread_mutex_lock(&(n->mtx));
         debugger(RUN_EVENT, *(n->p), 0);
+        //printf("Quanta = %g\n", w);
         sleepFor(w);
         n->p->dt -= w;
         debugger(EXIT_EVENT, *(n->p), 0);
@@ -48,7 +57,7 @@ void *runPScheduler(void *arg) {
 }
 
 double calculatePriority(Process p){
-    double qMult = 0.5;
+    //double qMult = 0.5;
     double priority = 0;
     double t0 = p.t0;
     double dt = p.dt;
@@ -60,9 +69,29 @@ double calculatePriority(Process p){
     if(punc > 0)
         priority = a*pow(punc, 2) + b*punc + c*dt + d*t0;
     // qqMult = -67*log10(pow(1+exp(priority/47.0),-1)); (max Quantum Multiplier = 20)
-    qMult = -33*log10(pow(1+exp(priority/47.0),-1)); // (max Quantum Multiplier = 10)
-    qMult = qMult < 0.5 ? 0.5 : qMult;
-    return 1;
+    //qMult = -33*log10(pow(1+exp(priority/47.0),-1)); // (max Quantum Multiplier = 10)
+    //qMult = qMult < 0.5 ? 0.5 : qMult;
+    return priority;
+}
+
+void addToStats(double priority) {
+    var = (count*(var + pow(avg, 2)) + pow(priority, 2))/(count + 1);
+    avg = (avg*count + priority)/(count + 1);
+    var -= pow(avg, 2);
+    count++;
+}
+
+void removeFromStats(double priority) {
+    if (count == 1) {
+        var = 0;
+        avg = 0;
+    }
+    else {
+        var = (count*(var + pow(avg, 2)) - pow(priority, 2))/(count - 1);
+        avg = (avg*count - priority)/(count - 1);
+        var -= pow(avg, 2);
+    }
+    count--;
 }
 
 static void wakeup_next(Queue q, Stack *s){
@@ -71,6 +100,7 @@ static void wakeup_next(Queue q, Stack *s){
     while (n && n->p->t0 <= timer->passed(timer)) {
         // set priority of process in quantum array
         quantum[n->p->nLine] = calculatePriority(*(n->p));
+        addToStats(quantum[n->p->nLine]);
         // Add new processes to queue if global time > t0
         queue_add(q, n);
         debugger(ARRIVAL_EVENT, *(n->p), 0);
@@ -81,8 +111,10 @@ static void wakeup_next(Queue q, Stack *s){
     // Readd the process to queue or remove it from queue
     if ((mem = queue_first(q)) && mem->p->dt)
         queue_readd(q);
-    else
+    else if (mem){
         queue_remove(q);
+        removeFromStats(quantum[mem->p->nLine]);
+    }
 
     // Start/restart the next process
     if ((n = queue_first(q)))
@@ -119,5 +151,5 @@ void schedulerPriority(ProcArray pQueue, char *outfile){
         pthread_mutex_lock(&gmtx);
         wakeup_next(runningP, pool);
     }
-    printf("Deadline completed = %lf%%\n",10.0*(double)deadlineCompleted/(double)pQueue->i);
+    printf("Deadline completed = %lf%%\n", 100.0*(double)deadlineCompleted/(double)pQueue->i);
 }
