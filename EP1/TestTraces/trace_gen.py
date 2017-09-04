75e75e30d3108d7d806314a1438495d524215f2c
robotenique <julianogarcia_1997@hotmail.com>
2017-09-04 20:16:17 -0300

    add lots of traces to test...

--- EP1/TestTraces/trace_gen.py
+++ EP1/TestTraces/trace_gen.py
@@ -1,10 +1,41 @@
 from sys import argv
 from random import random
+# Hardcoded distributions to generate
+trace_dist = [[[10, 13, 10, 2, 2],
+           [10, 13, 10, 10, 0.2],
+           [10, 13, 10, 7, 0.7]],
+          [[10, 73, 20, 10, 0.8],
+           [10, 73, 20, 8, 0.5],
+           [10, 73, 20, 40, 0.3]],
+          [[10, 157, 80, 20, 1],
+           [10, 157, 80, 5, 0.8],
+           [10, 157, 80, 10, 0.7]]
+         ]
+filename = ['small', 'med', 'long']
+def genHC():
+    tc = trace_dist
+    for category in range(len(tc)):
+        for dist in range(len(tc[category])):
+            files, lines, maxt0, maxdt, maxr = tc[category][dist]
+            for j in range(dist*10, dist*10 + 10):
+                with open(f"{filename[category]}trace{j}", "w+") as f:
+                    t0s = list()
+                    for i in range(lines):
+                        t0s.append(maxt0*random())
+                        t0s.sort()
+                    for i in range(lines):
+                        dt = maxdt*random()
+                        dl = t0s[i] + dt + dt*maxr*random()                
+                        f.write("{:.0f} {:.1f} {:.1f} processo{}\n".format(t0s[i], dt, dl, i))
+
 
 def main():
     if (len(argv) < 6):
-        print("Wrong number of arguments!!")
-        print("./trace_gen <number of files> <lines> <max t0> <max dt> <max deadline/dt ratio>")
+        if(len(argv) == 2 and argv[1] == '--giveall'):
+            genHC();
+        else:
+            print("Wrong number of arguments!!")
+            print("./trace_gen <number of files> <lines> <max t0> <max dt> <max deadline/dt ratio>")
         return
     files = int(argv[1])
     lines = int(argv[2])
@@ -19,7 +50,7 @@ def main():
             t0s.sort()
             for i in range(lines):
                 dt = maxdt*random()
-                dl = t0s[i] + maxr*maxdt*random()
+                dl = t0s[i] + dt + dt*maxr*random()                
                 f.write("{:.0f} {:.1f} {:.1f} processo{}\n".format(t0s[i], dt, dl, i))
 
 if __name__ == "__main__":
