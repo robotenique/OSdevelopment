54b6d32740b038fefb1fff9dc9b896edb460007b
robotenique <julianogarcia_1997@hotmail.com>
2017-09-06 14:17:38 -0300

    add some comments to priority scheduler

--- EP1/priorityScheduler.c
+++ EP1/priorityScheduler.c
@@ -15,177 +15,55 @@
 
 // TODO: DON'T LET THE SIMULATOR RUN PROCESSES WITH DT = 0....
 
-static Timer timer;
-static pthread_mutex_t gmtx;
-static double *quantum;
-static int finished = 0;
+static Timer timer; // global timer
+static pthread_t **ranThreads; // Storages the threads that we run
+static pthread_mutex_t gmtx; // global mutex
+static double *quantum; // array with the priority of each process
+static int finished = 0; // number of finished processes
 static double var = 0;
 static double avg = 0;
 static int count = 0;
-static bool SIGMOID = false;
-static pthread_t **ranThreads;
 
 // TODO: REMOVE EVERY MENTION OF THIS BUGGY THING AFTER STATISTICS ARE GENERATED!
 static bool* firstTime;
 // deadline related
 static deadlineC *deadArray;
+static bool SIGMOID = false;
 
-
-
-
-
-static void *iWait(void *t) {
-    double *dt = (double *)t;
-    sleepFor(*dt);
-    //printf("Esperei por %gs\n", *dt);
-    pthread_mutex_unlock(&gmtx);
-    return NULL;
-}
-
-double applyLogSigmoid(double priority){
-    //double qMult = -67*log10(pow(1+exp(-priority/47.0),-1)); // (max Quantum Multiplier = 20)
-    double qMult = -33*log10(pow(1+exp(-priority/25.0),-1)); // (max Quantum Multiplier = 10)
-    qMult = qMult < 1 ? 1 : qMult;
-    return qMult;
-}
-
-double calcQuanta(double priority) {
-    if(SIGMOID)
-        return applyLogSigmoid(priority);
-    double L = (!var)? 0 : (priority - avg)/sqrt(var);
-    double scale = 2.25*fmin(4.0, fabs(L));
-    printf("L = %g / scale = %g\n", L, scale);
-    return QUANTUM_VAL*(scale + 1);
-}
-
-void *runPScheduler(void *arg) {
-    Node *n = (Node *)arg;
-    double w;
-
-    deadlineC deadarr;
-    do {
-        pthread_mutex_lock(&(n->mtx));
-        debugger(RUN_EVENT, n->p, CPU_CORE);
-        if(firstTime[n->p->nLine]){
-            // The first time this process has run, it will save the waitTime...
-            firstTime[n->p->nLine] = false;
-            deadarr.waitTime = timer->passed(timer) - n->p->t0;
-        }
-        w = fmin(n->p->dt, calcQuanta(quantum[n->p->nLine]));
-        printf("Avg = %g / SD = %g\n", avg, sqrt(var));
-        printf("Priority = %g / Quanta = %g\n", quantum[n->p->nLine], w);
-        sleepFor(w);
-        n->p->dt -= w;
-        debugger(EXIT_EVENT, n->p, CPU_CORE);
-        pthread_mutex_unlock(&gmtx);
-    } while (n->p->dt);
-
-    finished++;
-    deadarr.realFinished = timer->passed(timer);
-    deadarr.deadline = n->p->deadline;
-    deadArray[n->p->nLine] = deadarr;
-    debugger(END_EVENT, n->p, finished);
-    write_outfile("%s %lf %lf\n", n->p->name, timer->passed(timer), timer->passed(timer) - n->p->t0);
-
-    return NULL;
-}
-
-double calculatePriority(Process p){
-    double priority = 5000;
-    double t0 = p.t0;
-    double dt = p.dt;
-    double punc = p.deadline - p.dt;
-    double d = 0.207715732988;
-    double c = 0.21137699282;
-    double b = 2.06241892813;
-    double a = 0.00213475762298;
-    if(punc > 0)
-        priority = a*pow(punc, 2) + b*punc + c*dt + d*t0;
-    return priority;
-}
-
-void addToStats(double priority) {
-    var = (count*(var + pow(avg, 2)) + pow(priority, 2))/(count + 1);
-    avg = (avg*count + priority)/(count + 1);
-    var -= pow(avg, 2);
-    if (isnan(var))
-        var = 0;
-    count++;
-}
-
-void removeFromStats(double priority) {
-    if (count == 1) {
-        var = 0;
-        avg = 0;
-    }
-    else {
-        var = (count*(var + pow(avg, 2)) - pow(priority, 2))/(count - 1);
-        avg = (avg*count - priority)/(count - 1);
-        var -= pow(avg, 2);
-    }
-    count--;
-}
-
-static void wakeup_next(Queue q, Stack *s){
-    Node *n = stack_top(s);
-    Node *mem = NULL;
-    Node *notEmpty = queue_first(q);
-    while (n && n->p->t0 <= timer->passed(timer)) {
-        // set priority of process in quantum array
-        quantum[n->p->nLine] = calculatePriority(*(n->p));
-        addToStats(quantum[n->p->nLine]);
-        // Add new processes to queue if global time > t0
-        queue_add(q, n);
-        debugger(ARRIVAL_EVENT, n->p, 0);
-        stack_remove(s);
-        ranThreads[n->p->nLine] = &(n->t);
-        pthread_create(&(n->t), NULL, &runPScheduler, (void *)n);
-        n = stack_top(s);
-    }
-    // Readd the process to queue or remove it from queue
-    if (notEmpty) {
-        if ((mem = queue_first(q)) && mem->p->dt)
-            queue_readd(q);
-        else if (mem){
-            queue_remove(q);
-            removeFromStats(quantum[mem->p->nLine]);
-        }
-    }
-
-    queue_debug(q);
-
-    // Start/restart the next process
-    if ((n = queue_first(q)))
-        pthread_mutex_unlock(&(n->mtx));
-    if (mem != n && n)
-        debugger(CONTEXT_EVENT, NULL, 0);
-}
+static void *iWait(void *);
+double applyLogSigmoid(double);
+double calcQuanta(double);
+void *runPScheduler(void *);
+double calculatePriority(Process p);
+void addToStats(double priority);
+void removeFromStats(double);
+static void wakeup_next(Queue, Stack *);
 
 void schedulerPriority(ProcArray pQueue){
-    int sz = pQueue->i + 1;
+    Node *tmp;
+    pthread_t idleThread;
+    int sz = pQueue->i + 1; // size auxiliar variable
     // TODO: choose between one model... But test each of them
     SIGMOID = sigval;
-    printf("RODANDO COM SIGMOID? %d\n",SIGMOID);
-    Stack *pool = new_stack(pQueue->i);
-    Queue runningP = new_queue();
-    pthread_t idleThread;
-    Node *tmp;
-    quantum = emalloc(sizeof(double)*sz);
-    deadArray = emalloc(sizeof(deadlineC)*sz);
+    Stack *pool = new_stack(pQueue->i); // Pool for arriving processes
+    Queue runningP = new_queue(); // Queue of processes to run at the moment
+    quantum = emalloc(sizeof(double)*sz); // Array with the priority of each process
     ranThreads = emalloc(sizeof(pthread_t*)*sz);
     firstTime = emalloc(sizeof(bool)*sz);
     for(int i = 0; i < sz; firstTime[i] = true,  i++);
+    deadArray = emalloc(sizeof(deadlineC)*sz);
 
     timer = new_Timer();
     bool notIdle = true;
 
-    // Transfer processes to stack
+    // Transfer processes to stack pool
     for (int i = pQueue->i - 1; i >= 0; i--)
         pool->v[pQueue->i - i - 1].p = &(pQueue->v[i]);
 
     pthread_mutex_init(&gmtx, NULL);
     pthread_mutex_lock(&gmtx);
 
+    // Initiate all the mutexes
     for (int i = 0; i < pQueue->i; i++) {
         pthread_mutex_init(&(pool->v[i].mtx), NULL);
         pthread_mutex_lock(&(pool->v[i].mtx));
@@ -199,7 +77,6 @@ void schedulerPriority(ProcArray pQueue){
                 break;
             // Wait in idle mode if queue is empty
             double wt = tmp->p->t0 - timer->passed(timer);
-            //printf("Esperando processos chegarem...\n");
             ranThreads[0] = &idleThread;
             notIdle = false;
             pthread_create(&idleThread, NULL, &iWait, &wt);
@@ -259,4 +136,191 @@ void schedulerPriority(ProcArray pQueue){
     printf("Mudanças de contexto = %d\n", get_ctx_changes());
     printf("Tempo de espera médio = %lf||%%\n", avgWaittime);
     free(deadArray);
+}
+
+/*
+ * Function: runPScheduler
+ * --------------------------------------------------------
+ * The function that each process run. The processes will run
+ * this for the quanta calculated for the processes, base on
+ * the priority previously calculated.
+ * When it has slept for this calculated time, it will unlock
+ * the thread, for other processes to run.
+ *
+ * @args args : the node of the process
+ *
+ * @return
+ */
+void *runPScheduler(void *arg) {
+    Node *n = (Node *)arg;
+    double w;
+
+    deadlineC deadarr;
+    do {
+        pthread_mutex_lock(&(n->mtx));
+        debugger(RUN_EVENT, n->p, CPU_CORE);
+        if(firstTime[n->p->nLine]){
+            firstTime[n->p->nLine] = false;
+            deadarr.waitTime = timer->passed(timer) - n->p->t0;
+        }
+        // It will always run the minimum to complete, to not waste cpu time
+        w = fmin(n->p->dt, calcQuanta(quantum[n->p->nLine]));
+        printf("Avg = %g / SD = %g\n", avg, sqrt(var));
+        printf("Priority = %g / Quanta = %g\n", quantum[n->p->nLine], w);
+        sleepFor(w);
+        n->p->dt -= w;
+        debugger(EXIT_EVENT, n->p, CPU_CORE);
+        pthread_mutex_unlock(&gmtx);
+    } while (n->p->dt);
+
+    finished++;
+    deadarr.realFinished = timer->passed(timer);
+    deadarr.deadline = n->p->deadline;
+    deadArray[n->p->nLine] = deadarr;
+    debugger(END_EVENT, n->p, finished);
+    write_outfile("%s %lf %lf\n", n->p->name, timer->passed(timer), timer->passed(timer) - n->p->t0);
+
+    return NULL;
+}
+/*
+ * Function: calculatePriority
+ * --------------------------------------------------------
+ * Calculate the priority of a given process. The function to
+ * calculate the priority of the process depends on the t0
+ * of the process, the dt of the processes, and on a new value
+ * called 'punctuality', which is how much time more we have to
+ * complete the process until the deadline, or the "tightness"
+ * of this interval. As we choose this to be a very important factor,
+ * the priority depends linearly and quadratically on this factor.
+
+ * @args  p :  a process
+ *
+ * @return  a double representing the priority of the process. The
+ *          lower this number, the more priority it will have to run.
+ */
+double calculatePriority(Process p){
+    // Initialize the priority with a high value
+    double priority = 5000;
+    double t0 = p.t0;
+    double dt = p.dt;
+    double punc = p.deadline - p.dt;
+    /* These constants were calculated using machine learning
+     * and gradient descent on a base of values that we estipulated.
+     * The graph of this function can be visualized in the documents.
+    */
+    double d = 0.207715732988;
+    double c = 0.21137699282;
+    double b = 2.06241892813;
+    double a = 0.00213475762298;
+    /* If a process has punc < 0, it's impossible to complete it
+     * before it's deadline, so we give it the lowest priority to
+     * run (the 5000 that we initialized)
+     */
+    if(punc > 0)
+        priority = a*pow(punc, 2) + b*punc + c*dt + d*t0;
+    return priority;
+}
+
+
+double calcQuanta(double priority) {
+    if(SIGMOID)
+        return applyLogSigmoid(priority);
+    double L = (!var)? 0 : (priority - avg)/sqrt(var);
+    double scale = 2.25*fmin(4.0, fabs(L));
+    printf("L = %g / scale = %g\n", L, scale);
+    return QUANTUM_VAL*(scale + 1);
+}
+
+
+/*
+ * Function: applyLogSigmoid
+ * --------------------------------------------------------
+ * Receives a priority, and apply a sigmoid of the priority, then
+ * apply a negative log function to the result. This will give
+ * a quantum multiplier, from 1 to 10, according to the
+ * priority received.
+ *
+ * @args  priority :  the priority of the process
+ *
+ * @return  returned value
+ */
+double applyLogSigmoid(double priority){
+    double qMult = -33*log10(pow(1+exp(-priority/25.0),-1)); // (max Quantum Multiplier = 10)
+    // Don't use something less than 1 quantum multiplier, to be fair with everyone
+    qMult = qMult < 1 ? 1 : qMult;
+    return qMult;
+}
+
+void addToStats(double priority) {
+    var = (count*(var + pow(avg, 2)) + pow(priority, 2))/(count + 1);
+    avg = (avg*count + priority)/(count + 1);
+    var -= pow(avg, 2);
+    if (isnan(var))
+        var = 0;
+    count++;
+}
+
+void removeFromStats(double priority) {
+    if (count == 1) {
+        var = 0;
+        avg = 0;
+    }
+    else {
+        var = (count*(var + pow(avg, 2)) - pow(priority, 2))/(count - 1);
+        avg = (avg*count - priority)/(count - 1);
+        var -= pow(avg, 2);
+    }
+    count--;
+}
+
+/*
+ * Function: iWait
+ * --------------------------------------------------------
+ * Wait for a given time the scheduler
+ *
+ * @args  t :  a double *, the time in seconds to wait
+ *
+ * @return
+ */
+static void *iWait(void *t) {
+    double *dt = (double *)t;
+    sleepFor(*dt);
+    //printf("Esperei por %gs\n", *dt);
+    pthread_mutex_unlock(&gmtx);
+    return NULL;
+}
+
+static void wakeup_next(Queue q, Stack *s){
+    Node *n = stack_top(s);
+    Node *mem = NULL;
+    Node *notEmpty = queue_first(q);
+    while (n && n->p->t0 <= timer->passed(timer)) {
+        // set priority of process in quantum array
+        quantum[n->p->nLine] = calculatePriority(*(n->p));
+        addToStats(quantum[n->p->nLine]);
+        // Add new processes to queue if global time > t0
+        queue_add(q, n);
+        debugger(ARRIVAL_EVENT, n->p, 0);
+        stack_remove(s);
+        ranThreads[n->p->nLine] = &(n->t);
+        pthread_create(&(n->t), NULL, &runPScheduler, (void *)n);
+        n = stack_top(s);
+    }
+    // Readd the process to queue or remove it from queue
+    if (notEmpty) {
+        if ((mem = queue_first(q)) && mem->p->dt)
+            queue_readd(q);
+        else if (mem){
+            queue_remove(q);
+            removeFromStats(quantum[mem->p->nLine]);
+        }
+    }
+
+    queue_debug(q);
+
+    // Start/restart the next process
+    if ((n = queue_first(q)))
+        pthread_mutex_unlock(&(n->mtx));
+    if (mem != n && n)
+        debugger(CONTEXT_EVENT, NULL, 0);
 }
