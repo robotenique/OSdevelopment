6f5ea045e7d80d00dbe95b4a7de2c43d35ab9c40
robotenique <julianogarcia_1997@hotmail.com>
2017-11-13 21:24:18 -0200

    I think i've finished best fit....

--- EP3/memoryWriter.py
+++ EP3/memoryWriter.py
@@ -8,11 +8,14 @@ MAC0422
 This is the MemoryWriter object file.
 """
 
+MINUS_1 = 254
 class MemoryWriter(object):
-    def __init__(self, filename, page_size, ua_size):
+    def __init__(self, filename, page_size, ua_size, fsize):
         """Creates the instance and opens the file"""
         self.file = open(filename, "wb+")
         self.page_size = page_size
+        self.write(MINUS_1, 0, fsize)
+
 
     def write(self, pid, pos, size):
         """Writes a pid 'size' times, beginning at 'pos'"""
@@ -22,7 +25,7 @@ class MemoryWriter(object):
     def clean(self, page):
         """Cleans a page"""
         self.file.seek(page*self.page_size)
-        self.file.write(bytes((254 for i in range(self.page_size))))
+        self.file.write(bytes((MINUS_1 for i in range(self.page_size))))
 
     def close(self):
         """Closes the file"""
