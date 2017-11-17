9864903b1f797917dc94a8e1740e5bb5f8509e40
IceMage144 <jgabriel.basi@hotmail.com>
2017-11-17 17:43:04 -0200

    Add automatic generation of virtual and physical memory sizes at inputGenerator

--- EP3/inputGenerator.py
+++ EP3/inputGenerator.py
@@ -1,13 +1,12 @@
 import sys
 from random import randint, sample
+from math import ceil
 
 INPUT = [
-    #procs   comps   tf      max_dt   max_size   max_access
+    #ua       page_size  procs   comps   tf      max_dt   max_size   max_access
     #----------------------------------------------------
-    [10,     2,      20,     6,       10,        3]
+    [4,       8,         10,     2,      20,     6,       10,        3]
 ]
-
-MEM_INFOS = [50, 100, 4, 8]
 
 class Proc:
     ID = 0
@@ -24,7 +23,7 @@ class Proc:
             self.accesses.append(randint(0, self.size))
             self.accesses.append(i)
 
-    def process(self):
+    def is_process(self):
         return True
 
     def __str__(self):
@@ -34,7 +33,7 @@ class Compact:
     def __init__(self, tf, max_dt):
         self.t0 = randint(0, tf-max_dt)
 
-    def process(self):
+    def is_process(self):
         return False
 
     def __str__(self):
@@ -43,18 +42,40 @@ class Compact:
 def vec2str(v):
     return v.__str__().strip("[]").replace(",", "")
 
-def generate(num_procs, num_comps, tf, max_dt, max_size, max_access):
+def generate(ua, page_size, num_procs, num_comps, tf, max_dt, max_size, max_access):
     lines = []
     for i in range(num_procs):
         lines.append(Proc(tf, max_dt, max_size, max_access))
     for i in range(num_comps):
         lines.append(Compact(tf, max_dt))
-    return sorted(lines, key=lambda i: i.t0)
+    lines = sorted(lines, key=lambda i: i.t0)
+    pmax = 0
+    fmax = 0
+    for i in range(tf):
+        pages = 0
+        frames = 0
+        for j in range(len(lines)):
+            if (lines[j].is_process() and lines[j].t0 <= i and lines[j].tf >= i):
+                pages += ceil(lines[j].size/page_size)
+                for k in range(1, len(lines[j].accesses), 2):
+                    if (lines[j].accesses[k] == i):
+                        frames += 1
+        if (pages > pmax):
+            pmax = pages
+        if (frames > fmax):
+            fmax = frames
+    return pmax, fmax, lines
+
 
 def main():
-    f = open("input3.in", "w+")
-    f.write(vec2str(MEM_INFOS) + "\n")
-    for i in generate(*(INPUT[0])):
+    f = open("input4.in", "w+")
+    ua = INPUT[0][0]
+    page_size = INPUT[0][1]
+    pmax, fmax, lines = generate(*(INPUT[0]))
+    vsize = ceil(pmax*1.5)*page_size
+    psize = ceil(fmax*1.5)*page_size
+    f.write(f"{psize} {vsize} {ua} {page_size}\n")
+    for i in lines:
         f.write(i.__str__())
     f.close()
 
